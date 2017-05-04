7ca3f49eb1d64dd4e0ae12dca493a8ed91442cee
Sadot Cortes <sadotcortes@scoorm.me>
2017-05-04 12:01:50 -0500

    updated

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'public/index.html');
+const INDEX = path.join(__dirname, 'public', 'index.html');
 
 const server = express()
 	.use(express.static(path.join(__dirname)))
