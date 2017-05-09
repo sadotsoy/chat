ddedd2ea7d794572150cfa38aa83896605be4866
Sadot Cortes <sadotcortes@scoorm.me>
2017-05-09 11:26:00 -0500

    SaveUser to object created

--- server.js
+++ server.js
@@ -1,30 +1,3 @@
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// var path = require('path');
-
-// // Initialize appication with route / (that means root of the application)
-// app.get('/', function(req, res){
-//   var express=require('express');
-//   app.use(express.static(path.join(__dirname)));
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// // Register events on socket connection
-// io.on('connection', function(socket){
-// 	socket.on('chatMessage', function(from, msg){
-// 		io.emit('chatMessage', from, msg);
-//   });
-// 	socket.on('notifyUser', function(user){
-// 		io.emit('notifyUser', user);
-//   });
-// });
-
-// // Listen application request on port 3000
-// http.listen(3000, function() {
-
-// })
-
 'use strict';
 
 const express = require('express');
@@ -45,10 +18,11 @@ io.on('connection', (socket) => {
 	console.log('Chat Message')
 	socket.on('chatMessage', (from, msg) => {
 		io.emit('chatMessage', from, msg);
-		// setInterval(() => io.emit('chatMessage',from,msg),1000);
 	});
 	socket.on('notifyUser', (user) => {
 		io.emit('notifyUser', user);
-		// setInterval(() => io.emit('notifyUser',user),9000000);
+	});
+	socket.on('saveUser', (user) => {
+		io.emit('saveUser', user);
 	});
 });
